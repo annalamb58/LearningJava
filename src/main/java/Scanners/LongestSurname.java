1191c4ca5937d468286d28b6e7406dc1c60aa690
Anna <Anna@ZAKZRHB-CLLB22.ballito.local>
2024-09-19 07:50:03 +0200

    LongestSurname - preparing for prac inn term 4 it is wrong. Throws error. Unsure what is wrong, no red within code only failing when run

--- src/main/java/Scanners/LongestSurname.java
+++ src/main/java/Scanners/LongestSurname.java
@@ -13,20 +13,27 @@ import java.util.Scanner;
  * @author Anna
  */
 public class LongestSurname {
+
     public static void main(String[] args) throws FileNotFoundException {
-      File fileName = new File("names.txt");
+        File fileName = new File("data//names.txt");
         Scanner scMain = new Scanner(fileName);
-        
+
         String line = scMain.nextLine();
-        
+        String LongestName = "";
+        String currentSurname = "";
+
         while (scMain.hasNext()) {
-            Scanner scSurname = new Scanner(line) ;
+            Scanner scSurname = new Scanner(line);
             String surname = scSurname.next();
-            
-            
-            
+            currentSurname = surname;
+
+            if (currentSurname.length() > LongestName.length()) {
+                LongestName = currentSurname;
+            }
+
         }
         
-        
+        System.out.println("The LogestSurname is" + LongestName);
+
     }
 }
