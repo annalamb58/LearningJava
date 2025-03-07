97d0cbf9b31c8ab42f8f7ffa75b33ff393585f14
anna <anna@ZAKZRHB-CLLB9.ballito.local>
2025-03-07 09:44:23 +0200

    New classwork working with Arrays

--- src/main/java/Arrays/AsendingArrays.java
+++ src/main/java/Arrays/AsendingArrays.java
@@ -9,5 +9,20 @@ package Arrays;
  * @author anna
  */
 public class AsendingArrays {
-    
-}
+
+    public static void main(String[] args) {
+        int ouput[] = {};
+        int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+        for (int i = 0; i < array.length; i++) {
+            int lowest = array[i];
+            int next = array[i+1];
+            
+            if (lowest > next) {
+                lowest = next;
+            }//if
+            output[] = output[] + lowest;
+        } //for
+       
+    }//psvm
+}//main
