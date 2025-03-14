497da226b57a60d25c1e174f544695ba70d03b08
Anna <Anna@ZAKZRHB-CLLB9.ballito.local>
2025-03-14 16:29:52 +0200

    Working with Arrays -linear search

---
+++ src/main/java/Arrays/LinearSearch.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Arrays;
+
+/**
+ *
+ * @author Anna
+ */
+public class LinearSearch {
+//14/03
+    public static void main(String[] args) {
+        int[] array = {1, 6, 5, 99, 45};
+        //public static int theLinearSearch(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) { // beause value is from og method
+                //return[i];
+            }
+        } // for
+        //return -1;
+        // }// theLinearSearch method
+    }//psvm
+} //class
+
+
+//Questions
+// how to write method
+// does the method stay within the main method
+// do methods have cap letters
